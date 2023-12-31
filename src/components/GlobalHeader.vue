0d8fe4f15abdf728f87fb8749c85d05254086a7d
迷寂 <2408893294@qq.com>
2023-12-31 18:43:30 +0800

    第一次提交

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,80 @@
+<template>
+  <a-row
+    id="globalHeader"
+    class="grid-demo"
+    style="margin-bottom: 16px"
+    align="center"
+  >
+    <a-col flex="auto">
+      <a-menu
+        mode="horizontal"
+        :selected-keys="selectedKeys"
+        @menu-item-click="doMenuClick"
+      >
+        <a-menu-item
+          key="0"
+          :style="{ padding: 0, marginRight: '38px' }"
+          disabled
+        >
+          <div class="titleBar">
+            <img class="logo" src="../assets/zyx.png" />
+            <div class="title">zyx OJ</div>
+          </div>
+        </a-menu-item>
+        <a-menu-item v-for="item in routes" :key="item.path">
+          {{ item.name }}
+        </a-menu-item>
+      </a-menu>
+    </a-col>
+    <a-col flex="100px">
+      <div>{{ store.state.user.loginUser.userName }}</div>
+    </a-col>
+  </a-row>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+import { routes } from "@/router/routes";
+import { ref } from "vue";
+import { useStore } from "vuex";
+
+const router = useRouter();
+//默认主页
+const selectedKeys = ref(["/"]);
+
+//路由跳转后，更新选中的菜单项
+router.afterEach((to, from, failure) => {
+  selectedKeys.value = [to.path];
+});
+const doMenuClick = (key: string) => {
+  router.push({
+    path: key,
+  });
+};
+
+// setTimeout(() => {
+//   store.dispatch("user/getLoginUser", {
+//     userName: "zyx",
+//     role: "admin",
+//   });
+// }, 3000);
+
+const store = useStore();
+</script>
+
+<style scoped>
+.titleBar {
+  display: flex;
+  align-items: center;
+}
+
+.title {
+  color: #444;
+  font-size: medium;
+  margin-left: 16px;
+}
+
+.logo {
+  width: 52px;
+}
+</style>
